7671f7467696ddd1dc76001486d0bbd55a04c45b
takwai <takwai0318@gmail.com>
2022-05-26 19:17:37 -0400

    update homepage

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -13,7 +13,7 @@ export default function PokemonCard({ id, name, sprites, withDeleteButton, handl
   return (
 
     <Card className="pokemonCard" key={id} data-id={id}>
-        (<Image src={sprites.front_default} onClick={handleClick} />)
+        <Image src={sprites.front_default} onClick={handleClick} />
       <Card.Content>
         <Card.Header textAlign="center">{name.split(" ").map(letter => letter.charAt(0).toUpperCase() + letter.substring(1)).join()}</Card.Header>
       </Card.Content>
