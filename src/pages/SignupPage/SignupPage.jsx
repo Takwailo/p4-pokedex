0bf233b9a94cc2ef2b37c6de856e8b0e795e3f0d
takwai <takwai0318@gmail.com>
2022-05-27 15:25:41 -0400

    sign up console.log

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -47,7 +47,7 @@ export default function SignUpPage(props) {
       navigate('/')
 
     } catch(err){
-      console.log(err.message);
+      console.log(err);
       setError(err.message)
     }
 
--- src/utils/userService.js
+++ src/utils/userService.js
@@ -1,7 +1,7 @@
 import tokenService from "./tokenService";
 
 const BASE_URL = "/api/users/";
-
+  
 // NOTE THIS IS configured to send of a multi/part form request
 // aka photo
 function signup(user) {
